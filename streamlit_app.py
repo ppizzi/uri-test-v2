689abe2b27285edb7137b42131544462f1a2e021
ppizzi <43932233+ppizzi@users.noreply.github.com>
2025-01-31 11:26:54 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -96,12 +96,12 @@ with open("uri_test_reference.jpeg", "rb") as f:
 #--upload test strip photo, rotate it, save it
 col1, col2 = st.columns(2)
 up_image=st.file_uploader("Upload your photo", type=["jpeg", "png"])
-#up_image_bytes = up_image.read()
-up_image_Img = ImageOps.exif_transpose(up_image) 
+up_image_bytes = up_image.read()
+#up_image_Img = ImageOps.exif_transpose(up_image) 
 
 if up_image is not None:
     col1.image(ref_image)
-    col2.image(up_image_Img)
-    #call_llm(ref_image, up_image_bytes)
+    col2.image(up_image)
+    call_llm(ref_image, up_image_bytes)
 
 
