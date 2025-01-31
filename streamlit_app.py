84ebd3eda6b65820cf97715b5cd162962cb065c7
ppizzi <43932233+ppizzi@users.noreply.github.com>
2025-01-31 10:41:20 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,48 @@
-import streamlit as st
+    # inspiration: 
+# https://github.com/aarushdixit889/photo-semantics-analyzer/blob/main/app.py
+# https://medium.com/@codingmatheus/sending-images-to-claude-3-using-amazon-bedrock-b588f104424f
+# https://docs.streamlit.io/develop/tutorials/chat-and-llm-apps/build-conversational-apps
+# https://github.com/awsdocs/aws-doc-sdk-examples/blob/main/python/example_code/bedrock-runtime/models/anthropic_claude/converse.py#L4
+# https://docs.aws.amazon.com/nova/latest/userguide/modalities-image-examples.html
+# https://discuss.streamlit.io/t/unique-key-for-every-items-in-radio-button/20654/4
+# https://stackoverflow.com/questions/3715493/encoding-an-image-file-with-base64
+# https://docs.aws.amazon.com/bedrock/latest/APIReference/API_runtime_ImageSource.html
+# https://docs.aws.amazon.com/bedrock/latest/APIReference/API_runtime_Message.html
+# https://docs.anthropic.com/en/docs/build-with-claude/vision
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+
+import streamlit as st
+import json
+from PIL import Image
+import PIL
+import os
+import base64
+import boto3
+from botocore.exceptions import ClientError
+
+
+# -- functions --
+#--- end of function definition ---
+
+
+####--- main page ---###
+st.title(":pill: Urine Test Analysis v2")
+st.write("Upload a photo of your urine test strip for analysis")
+
+#--Select model for inference
+# naming conventions: https://docs.aws.amazon.com/bedrock/latest/userguide/models-supported.html
+model_ids = ["us.anthropic.claude-3-5-sonnet-20240620-v1:0", "amazon.nova-lite-v1:0"]
+model_id = model_ids[0] 
+st.write("\(note: this app uses the following LLM model: ", model_id, "\)" )
+
+#--Create a Bedrock Runtime client in the AWS Region you want to use.
+client = boto3.client(
+    'bedrock-runtime',
+    aws_access_key_id=st.secrets["AWS_ACCESS_KEY_ID"],
+    aws_secret_access_key=st.secrets["AWS_SECRET_ACCESS_KEY"],
+    region_name=st.secrets["AWS_REGION"]
 )
+
+#--select user language
+output_language = st.radio("Select your language:", ["Spanish","Italian","English"]) 
+st.write("You selected: ", output_language)
